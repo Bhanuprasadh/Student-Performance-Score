c4fe97f411d28dc1b482a04f3b9aa87df2284d6f
Bhanuprasadh Santra <bhanuprasadh@users.noreply.github.com>
2025-10-30 10:37:43 +0530

    Update exception.py

--- src/exception.py
+++ src/exception.py
@@ -1,17 +1,24 @@
 import sys
 
-def error_msg_detail(error, error_detail: sys):
-    _, _, exc_tb = error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    error_msg = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name, exc_tb.tb_lineno, str(error)
-    )  
-    return error_msg  
+
+def error_msg_detail(error):
+    _, _, exc_tb = sys.exc_info() 
+    
+    if exc_tb is not None:
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_num = exc_tb.tb_lineno
+        error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
+            file_name, line_num, str(error)
+        )
+        return error_message
+    else:
+        return str(error)
+
 
 class CustomException(Exception):
-    def __init__(self, error_msg, error_detail: sys):
-        super().__init__(error_msg)  
-        self.error_msg = error_msg_detail(error_msg, error_detail=error_detail)
+    def __init__(self, error_message):
+        super().__init__(error_message)
+        self.error_msg = error_message
     
     def __str__(self):
         return self.error_msg
