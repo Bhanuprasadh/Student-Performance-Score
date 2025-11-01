5df1c56408841c9ca3c2c184bcf1a22c79bfa590
Bhanuprasadh Santra <bhanuprasadh@users.noreply.github.com>
2025-11-01 11:04:09 +0530

    Update data_ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,3 +1,4 @@
+from encodings.punycode import T
 import os
 import sys
 from src.exception import CustomException
@@ -23,5 +24,26 @@ class DataIngestion:
         try:
             df = pd.read_csv('notebook/data/stud.csv')
             logging.info("Read the Dataset as Dataframe")
-        except:
-            pass 
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+
+            logging.info("Train Test split initiated")
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+
+            logging.info("Data Ingestion Completed")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
