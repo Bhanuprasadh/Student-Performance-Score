9364eb6174d0f508d03a95a071380126a268879c
Bhanuprasadh Santra <bhanuprasadh@users.noreply.github.com>
2025-11-07 12:53:59 +0530

    Updated components

--- src/logger.py
+++ src/logger.py
@@ -2,22 +2,16 @@ import logging
 import os
 from datetime import datetime
 
-# 1. Define the filename with timestamp
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
 
-# 2. Define the directory path (without the filename)
-LOGS_DIR = os.path.join(os.getcwd(), "logs")
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
-# 3. Create the directory (only the directory)
-# This will create the 'logs' folder if it doesn't exist
-os.makedirs(LOGS_DIR, exist_ok=True)
-
-# 4. Define the full log file path
-LOG_FILE_PATH = os.path.join(LOGS_DIR, LOG_FILE)
-
-# 5. Configure the logging
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
+
+
 )
